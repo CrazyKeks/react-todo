eb2b1c3440eac82bd1f1e3cbbca4fa48a94fbb04
crazykeks <voloshinwoolf24@gmail.com>
2020-07-06 00:04:43 +0300

    Тест хуков

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import TodoItem from "./TodoItem";
 import {TodoWrap} from "./styled";
 import {DefTitle} from "../../assets/title/styled";
@@ -7,99 +7,88 @@ import {DefInput} from "../../assets/input/styled";
 import {InputWrapper} from "../../assets/wrapper/styled";
 import './style.css'
 
-class Todo extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            currentValue: '',
-            editTextTask: '',
-            listTodo: [
-                {
-                    id: 1,
-                    text: 'test',
-                    edit: false,
-                }
-            ]
-        };
+function Todo() {
+    let state = {
+        currentValue: '',
+        editTextTask: '',
+        listTodo: [
+            {
+                id: 1,
+                text: 'test',
+                edit: false,
+            }
+        ]
+    };
 
-        this.addTask = this.addTask.bind(this);
-        this.selfValue = this.selfValue.bind(this);
-        this.editTask = this.editTask.bind(this);
-        this.removeTask = this.removeTask.bind(this);
-        this.saveTask = this.saveTask.bind(this);
-        this.cancelTask = this.cancelTask.bind(this);
-        this.editTextTaskValue = this.editTextTaskValue.bind(this);
-    }
-
-    editTask(id,text){
-        let newDataList = [...this.state.listTodo];
+    function editTask(id,text){
+        let newDataList = [...todoState.listTodo];
         newDataList.map((item)=> {
             if (item.id === id) {
                 item.edit = true;
             }
         });
-        this.setState({
+        updateStateTodo({
             listTodo: newDataList,
             editTextTask: text
         });
     }
 
-    removeTask(id) {
-        let newDataList = [...this.state.listTodo];
+    function removeTask(id) {
+        let newDataList = [...todoState.listTodo];
         newDataList.map((item, index)=>{
             if (item.id === id) {
                 newDataList.splice(index, 1)
             }
         });
-        this.setState({listTodo: newDataList});
+        updateStateTodo({listTodo: newDataList});
     }
 
-    cancelTask(id){
-        let newDataList = [...this.state.listTodo];
+    function cancelTask(id){
+        let newDataList = [...todoState.listTodo];
         newDataList.map((item)=>{
             if (item.id === id) {
                 item.edit = false;
             }
         });
-        this.setState({
+        updateStateTodo({
             listTodo: newDataList,
             editTextTask: ''
         });
     }
 
-    saveTask(id) {
-        let newDataList = [...this.state.listTodo];
+    function saveTask(id) {
+        let newDataList = [...todoState.listTodo];
         newDataList.map((item)=>{
             if (item.id === id) {
-                item.text = this.state.editTextTask;
+                item.text = todoState.editTextTask;
                 item.edit = false;
             }
         });
-        this.setState({
+        updateStateTodo({
             editTextTask: '',
             listTodo: newDataList
         });
     }
 
-    selfValue(event) {
-        this.setState({currentValue: event.target.value})
+    function selfValue(event) {
+        updateStateTodo({currentValue: event.target.value})
     }
 
-    editTextTaskValue(event) {
-        this.setState({editTextTask: event.target.value})
+    function editTextTaskValue(event) {
+        updateStateTodo({editTextTask: event.target.value})
     }
 
-    addTask(event) {
+    function addTask(event) {
         event.preventDefault();
-        const value = this.state.currentValue;
+        const value = todoState.currentValue;
         if (value) {
-            let newDataList = [...this.state.listTodo],
+            let newDataList = [...todoState.listTodo],
                 lastIndex;
 
-            if (this.state.listTodo.length === 0) {
+            if (todoState.listTodo.length === 0) {
                 lastIndex = 0;
             } else {
-                lastIndex = this.state.listTodo[this.state.listTodo.length - 1].id;
+                lastIndex = todoState.listTodo[todoState.listTodo.length - 1].id;
             }
 
             newDataList.push(
@@ -109,52 +98,49 @@ class Todo extends React.Component{
                     edit: false,
                 }
             );
-            this.setState({
+            updateStateTodo({
                 currentValue: '',
                 listTodo:newDataList
             })
         }
     }
 
-    render() {
-        let {listTodo} = this.state;
-        return (
-            <div className="todo">
-                <DefTitle className="todo__title">Todo лист</DefTitle>
-                <form className="todo__form" onSubmit={this.addTask}>
-                    <div className="todo__enter-wrap">
-                        <InputWrapper margin={'0 10px 0 0'}>
-                            <DefInput
-                                type="text"
-                                className="todo__enter"
-                                placeholder='Введите запись'
-                                onChange ={this.selfValue}
-                                value={this.state.currentValue}
+    const [todoState, updateStateTodo] = useState(state);
+    return (
+        <div className="todo">
+            <DefTitle className="todo__title">Todo лист</DefTitle>
+            <form className="todo__form" onSubmit={addTask}>
+                <div className="todo__enter-wrap">
+                    <InputWrapper margin={'0 10px 0 0'}>
+                        <DefInput
+                            type="text"
+                            className="todo__enter"
+                            placeholder='Введите запись'
+                            onChange ={selfValue}
+                            value={todoState.currentValue}
+                        />
+                    </InputWrapper>
+                    <DefButton className="todo__btn-submit">Добавить</DefButton>
+                </div>
+                <TodoWrap className="todo__list">
+                    {
+                        todoState.listTodo.map((item)=>{
+                            return <TodoItem
+                                key={item.id}
+                                id={item.id}
+                                text={item.text}
+                                edit={item.edit}
+                                onEdit={()=>editTask(item.id, item.text)}
+                                onRemove={()=>removeTask(item.id)}
+                                onEditText={editTextTaskValue}
+                                onSaveText={()=>saveTask(item.id)}
+                                onCancelEdit={()=>cancelTask(item.id)}
                             />
-                        </InputWrapper>
-                        <DefButton className="todo__btn-submit">Добавить</DefButton>
-                    </div>
-                    <TodoWrap className="todo__list">
-                      {
-                        listTodo.map((item)=>{
-                          return <TodoItem
-                              key={item.id}
-                              id={item.id}
-                              text={item.text}
-                              edit={item.edit}
-                              onEdit={()=>this.editTask(item.id, item.text)}
-                              onRemove={()=>this.removeTask(item.id)}
-                              onEditText={this.editTextTaskValue}
-                              onSaveText={()=>this.saveTask(item.id)}
-                              onCancelEdit={()=>this.cancelTask(item.id)}
-                          />
                         })
-                      }
-                    </TodoWrap>
-                </form>
-            </div>
-        )
-    }
+                    }
+                </TodoWrap>
+            </form>
+        </div>
+    )
 }
-
 export default Todo;
